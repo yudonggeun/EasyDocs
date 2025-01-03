78e87182537e3d1758ee50731405abc1b5795960
yudonggeun <ydong98@gmail.com>
2025-01-03 22:12:56 +0900

    HttpRequestForm 추가
    - HttpRequestForm은 http request 요청 스키마와 값을 모두 조회할 수 있는 데이터 클래스입니다.
    - HeaderForm은 header의 이름과 값을 조회할 수 있는 데이터 클래스입니다.

---
+++ src/main/java/io/github/yudonggeun/http/form/HeaderForm.java
@@ -0,0 +1,40 @@
+package io.github.yudonggeun.http.form;
+
+import io.github.yudonggeun.http.JsonType;
+
+public class HeaderForm {
+
+    private final String name;
+    private final String description;
+    private final boolean required;
+    private final JsonType type;
+    private final Object value;
+
+    public HeaderForm(String name, String description, boolean required, JsonType type, Object value) {
+        this.name = name;
+        this.description = description;
+        this.required = required;
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public JsonType getType() {
+        return type;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/io/github/yudonggeun/http/form/HttpRequestForm.java
@@ -0,0 +1,106 @@
+package io.github.yudonggeun.http.form;
+
+import io.github.yudonggeun.http.HttpMethod;
+import io.github.yudonggeun.http.JsonType;
+import io.github.yudonggeun.http.annotation.HeaderSpec;
+import io.github.yudonggeun.http.annotation.RequestSpec;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class HttpRequestForm {
+
+    private Object request;
+    private HttpMethod method;
+    private String url;
+    private List<HeaderForm> headers;
+
+    public HttpRequestForm(Object request) {
+        Class<?> clazz = request.getClass();
+        if (!clazz.isAnnotationPresent(RequestSpec.class))
+            throw new IllegalArgumentException("the input must be an instance of a class with the RequestSpec annotation.");
+
+        this.request = request;
+        initStartLine();
+        initHeaders();
+    }
+
+    private void initStartLine() {
+        Class<?> clazz = request.getClass();
+        RequestSpec requestSpec = clazz.getAnnotation(RequestSpec.class);
+        this.method = requestSpec.method();
+        this.url = requestSpec.url();
+    }
+
+    private void initHeaders() {
+
+        headers = new ArrayList<>();
+        Class<?> clazz = request.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(HeaderSpec.class)) {
+                HeaderSpec spec = field.getAnnotation(HeaderSpec.class);
+                field.setAccessible(true);
+
+                String name = spec.name();
+                String description = spec.description();
+                boolean required = spec.required();
+                JsonType type = spec.type();
+
+                try {
+                    Object value = field.get(request);
+
+                    if (name.isEmpty()) {
+                        name = field.getName();
+                    }
+                    if (type == JsonType.NULL) {
+                        Class<?> fieldType = field.getType();
+                        if (fieldType.equals(String.class)) {
+                            type = JsonType.STRING;
+                        } else if (fieldType.equals(Integer.class) || fieldType.equals(int.class) ||
+                                fieldType.equals(Long.class) || fieldType.equals(long.class) ||
+                                fieldType.equals(Double.class) || fieldType.equals(double.class) ||
+                                fieldType.equals(Float.class) || fieldType.equals(float.class)) {
+                            type = JsonType.NUMBER;
+                        } else if (fieldType.equals(Boolean.class) || fieldType.equals(boolean.class)) {
+                            type = JsonType.BOOLEAN;
+                        } else if (fieldType.isArray()) {
+                            type = JsonType.ARRAY;
+                        } else {
+                            type = JsonType.OBJECT;
+                        }
+                    }
+
+                    headers.add(new HeaderForm(name, description, required, type, value));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("HeaderSpec is not available on static fields.", e);
+                }
+            }
+        }
+    }
+
+    public String getUrl() {
+        Class<?> clazz = request.getClass();
+        RequestSpec requestSpec = clazz.getAnnotation(RequestSpec.class);
+        return requestSpec.url();
+    }
+
+    public HttpMethod getMethod() {
+        Class<?> clazz = request.getClass();
+        RequestSpec requestSpec = clazz.getAnnotation(RequestSpec.class);
+        return requestSpec.method();
+    }
+
+    public Optional<HeaderForm> getHeader(String name) {
+        return headers.stream()
+                .filter(header -> header.getName().equals(name))
+                .findFirst();
+    }
+
+    public List<HeaderForm> getHeaders() {
+        return headers;
+    }
+}
