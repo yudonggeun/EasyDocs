a86440c0dd0d573d9a10c11e48f03bd71acbfc26
yudonggeun <ydong98@gmail.com>
2025-01-03 22:10:14 +0900

    HeaderSpec 속성 추가

--- src/main/java/io/github/yudonggeun/http/annotation/HeaderSpec.java
+++ src/main/java/io/github/yudonggeun/http/annotation/HeaderSpec.java
@@ -1,5 +1,7 @@
 package io.github.yudonggeun.http.annotation;
 
+import io.github.yudonggeun.http.JsonType;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,4 +10,12 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface HeaderSpec {
+
+    String name() default "";
+
+    String description() default "";
+
+    boolean required() default false;
+
+    JsonType type() default JsonType.NULL;
 }
