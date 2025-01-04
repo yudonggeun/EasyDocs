c7bbc4a701c17d68f859c36bcac68515297ad1f5
yudonggeun <ydong98@gmail.com>
2025-01-04 10:46:37 +0900

    HttpRequestForm query parameter 속성 추가

--- src/main/java/io/github/yudonggeun/http/form/HttpRequestForm.java
+++ src/main/java/io/github/yudonggeun/http/form/HttpRequestForm.java
@@ -4,6 +4,7 @@ import io.github.yudonggeun.http.HttpMethod;
 import io.github.yudonggeun.http.JsonType;
 import io.github.yudonggeun.http.annotation.HeaderSpec;
 import io.github.yudonggeun.http.annotation.PathSpec;
+import io.github.yudonggeun.http.annotation.QuerySpec;
 import io.github.yudonggeun.http.annotation.RequestSpec;
 
 import java.lang.reflect.Field;
@@ -18,6 +19,7 @@ public class HttpRequestForm {
     private HttpMethod method;
     private String url;
     private List<PathForm> pathParams;
+    private List<QueryForm> queryParams;
     private List<HeaderForm> headers;
 
     public HttpRequestForm(Object request) {
@@ -28,7 +30,8 @@ public class HttpRequestForm {
         this.request = request;
         initStartLine();
         initHeaders();
-        initPaths();
+        initPathParams();
+        initQueryParams();
     }
 
     private void initStartLine() {
@@ -87,7 +90,7 @@ public class HttpRequestForm {
         headers = Collections.unmodifiableList(headers);
     }
 
-    private void initPaths() {
+    private void initPathParams() {
         pathParams = new ArrayList<>();
         Class<?> clazz = request.getClass();
         Field[] fields = clazz.getDeclaredFields();
@@ -114,6 +117,52 @@ public class HttpRequestForm {
             }
         }
         pathParams = Collections.unmodifiableList(pathParams);
+    }
+
+    private void initQueryParams() {
+
+        queryParams = new ArrayList<>();
+        Class<?> clazz = request.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(QuerySpec.class)) {
+                QuerySpec spec = field.getAnnotation(QuerySpec.class);
+                field.setAccessible(true);
+
+                String name = spec.name();
+                String description = spec.description();
+                boolean required = spec.required();
+
+                try {
+                    Object value = field.get(request);
+                    JsonType type = null;
+
+                    if (name.isEmpty()) {
+                        name = field.getName();
+                    }
+
+                    Class<?> fieldType = field.getType();
+                    if (fieldType.equals(String.class)) {
+                        type = JsonType.STRING;
+                    } else if (fieldType.equals(Integer.class) || fieldType.equals(int.class) ||
+                            fieldType.equals(Long.class) || fieldType.equals(long.class) ||
+                            fieldType.equals(Double.class) || fieldType.equals(double.class) ||
+                            fieldType.equals(Float.class) || fieldType.equals(float.class)) {
+                        type = JsonType.NUMBER;
+                    } else if (fieldType.equals(Boolean.class) || fieldType.equals(boolean.class)) {
+                        type = JsonType.BOOLEAN;
+                    } else if (fieldType.isArray()) {
+                        type = JsonType.ARRAY;
+                    }
+
+                    queryParams.add(new QueryForm(name, description, required, type, value));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("HeaderSpec is not available on static fields.", e);
+                }
+            }
+        }
+        queryParams = Collections.unmodifiableList(queryParams);
     }
 
     public String getUrl() {
@@ -146,5 +195,15 @@ public class HttpRequestForm {
 
     public List<PathForm> getPathParams() {
         return pathParams;
+    }
+
+    public Optional<QueryForm> getQueryParam(String name) {
+        return queryParams.stream()
+                .filter(queryParam -> queryParam.getName().equals(name))
+                .findFirst();
+    }
+
+    public List<QueryForm> getQueryParams() {
+        return queryParams;
     }
 }
