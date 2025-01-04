9fb7055c532e11d3ff7d86c0c435e14a343f5d6c
yudonggeun <ydong98@gmail.com>
2025-01-04 10:45:56 +0900

    QueryForm 데이터 클래스 생성

---
+++ src/main/java/io/github/yudonggeun/http/form/QueryForm.java
@@ -0,0 +1,40 @@
+package io.github.yudonggeun.http.form;
+
+import io.github.yudonggeun.http.JsonType;
+
+public class QueryForm {
+
+    private final String name;
+    private final String description;
+    private final boolean required;
+    private final JsonType jsonType;
+    private final Object value;
+
+    public QueryForm(String name, String description, boolean required, JsonType jsonType, Object value) {
+        this.name = name;
+        this.description = description;
+        this.required = required;
+        this.jsonType = jsonType;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public JsonType getJsonType() {
+        return jsonType;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
