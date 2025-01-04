5b9dbdcab13083556571f1b7f9595c68de16fa0c
yudonggeun <ydong98@gmail.com>
2025-01-04 10:26:15 +0900

    PathSpec(name, description) 속성 추가

--- src/main/java/io/github/yudonggeun/http/annotation/PathSpec.java
+++ src/main/java/io/github/yudonggeun/http/annotation/PathSpec.java
@@ -8,4 +8,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface PathSpec {
+
+    String name() default "";
+
+    String description() default "";
 }
