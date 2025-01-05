ca5d1990f5d4e39a518ce119c85cc4458cd7cb04
yudonggeun <ydong98@gmail.com>
2025-01-05 17:41:05 +0900

    - ResponseSpec 프로세서 기능 추가로 인한 네이밍 변경

--- easy-docs-http/src/main/java/io/github/yudonggeun/http/annotation/processor/HttpSpecProcessor.java
+++ easy-docs-http/src/main/java/io/github/yudonggeun/http/annotation/processor/HttpSpecProcessor.java
@@ -1,9 +1,7 @@
 package io.github.yudonggeun.http.annotation.processor;
 
 import com.squareup.javapoet.*;
-import io.github.yudonggeun.http.annotation.BodySpec;
-import io.github.yudonggeun.http.annotation.HttpRequestInput;
-import io.github.yudonggeun.http.annotation.RequestSpec;
+import io.github.yudonggeun.http.annotation.*;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
@@ -13,87 +11,98 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
 import javax.lang.model.type.TypeKind;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-@SupportedAnnotationTypes({"io.github.yudonggeun.http.annotation.RequestSpec",})
+@SupportedAnnotationTypes({
+        "io.github.yudonggeun.http.annotation.RequestSpec",
+        "io.github.yudonggeun.http.annotation.ResponseSpec",
+})
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-public class RequestSpecProcessor extends AbstractProcessor {
+public class HttpSpecProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (Element element : roundEnv.getElementsAnnotatedWith(RequestSpec.class)) {
-            String pacakageName = getPackageName(element);
-            String className = element.getSimpleName().toString();
-            List<? extends Element> enclosedElements = element.getEnclosedElements();
-
-            TypeSpec innerClassSpec = null;
-            List<FieldSpec> fieldSpecs = new ArrayList<>();
-            List<MethodSpec> methodSpecs = new ArrayList<>();
-
-            // static factory method
-            String requestFormClassName = getNewClassName(className);
-            MethodSpec factoryMethod = createFactoryMethod(requestFormClassName);
-            methodSpecs.add(factoryMethod);
-
-            for (Element enclosedElement : enclosedElements) {
-
-                // field and method chain
-                if (enclosedElement.getKind() == ElementKind.FIELD) {
-                    FieldSpec fieldSpec = createField(enclosedElement);
-                    fieldSpecs.add(fieldSpec);
-
-                    MethodSpec methodSpec = createSetter(enclosedElement, requestFormClassName);
-                    methodSpecs.add(methodSpec);
-                }
-
-                // body
-                if (enclosedElement.getAnnotation(BodySpec.class) != null) {
-                    // object
-                    if (enclosedElement.getKind() == ElementKind.CLASS) {
-                        innerClassSpec = createInnerClassSpec(enclosedElement);
-
-                        ClassName innerClassName = ClassName.bestGuess(getNewClassName(enclosedElement.getSimpleName().toString()));
-                        FieldSpec.Builder fieldSpecBuilder = FieldSpec.builder(innerClassName, "body", Modifier.PRIVATE);
-                        FieldSpec fieldSpec = fieldSpecBuilder.build();
-                        fieldSpecs.add(fieldSpec);
-
-                        MethodSpec.Builder methodSpec = MethodSpec.methodBuilder("body")
-                                .addModifiers(Modifier.PUBLIC)
-                                .addParameter(innerClassName, "body")
-                                .returns(ClassName.bestGuess(requestFormClassName))
-                                .addStatement("this.body = body")
-                                .addStatement("return this");
-
-                        if (enclosedElement.asType().getKind() == TypeKind.ARRAY) {
-                            methodSpec.varargs(true);
-                        }
-                        methodSpecs.add(methodSpec.build());
-                    }
-                }
-            }
-
-            TypeSpec.Builder classBuilder = TypeSpec.classBuilder(requestFormClassName)
-                    .addSuperinterface(HttpRequestInput.class)
-                    .addModifiers(Modifier.PUBLIC)
-                    .addFields(fieldSpecs)
-                    .addMethods(methodSpecs);
-
-            if (innerClassSpec != null) {
-                classBuilder.addType(innerClassSpec);
-            }
-
-            JavaFile javaFile = JavaFile.builder(pacakageName, classBuilder.build())
-                    .build();
-
-            try {
-                javaFile.writeTo(processingEnv.getFiler());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            createFormClass(element, HttpRequestInput.class);
+        }
+        for (Element element : roundEnv.getElementsAnnotatedWith(ResponseSpec.class)) {
+            createFormClass(element, HttpResponseInput.class);
         }
         return true;
+    }
+
+    private void createFormClass(Element element, Type superType) {
+        String pacakageName = getPackageName(element);
+        String className = element.getSimpleName().toString();
+        List<? extends Element> enclosedElements = element.getEnclosedElements();
+
+        TypeSpec innerClassSpec = null;
+        List<FieldSpec> fieldSpecs = new ArrayList<>();
+        List<MethodSpec> methodSpecs = new ArrayList<>();
+
+        // static factory method
+        String requestFormClassName = getNewClassName(className);
+        MethodSpec factoryMethod = createFactoryMethod(requestFormClassName);
+        methodSpecs.add(factoryMethod);
+
+        for (Element enclosedElement : enclosedElements) {
+
+            // field and method chain
+            if (enclosedElement.getKind() == ElementKind.FIELD) {
+                FieldSpec fieldSpec = createField(enclosedElement);
+                fieldSpecs.add(fieldSpec);
+
+                MethodSpec methodSpec = createSetter(enclosedElement, requestFormClassName);
+                methodSpecs.add(methodSpec);
+            }
+
+            // body
+            if (enclosedElement.getAnnotation(BodySpec.class) != null) {
+                // object
+                if (enclosedElement.getKind() == ElementKind.CLASS) {
+                    innerClassSpec = createInnerClassSpec(enclosedElement);
+
+                    ClassName innerClassName = ClassName.bestGuess(getNewClassName(enclosedElement.getSimpleName().toString()));
+                    FieldSpec.Builder fieldSpecBuilder = FieldSpec.builder(innerClassName, "body", Modifier.PRIVATE);
+                    FieldSpec fieldSpec = fieldSpecBuilder.build();
+                    fieldSpecs.add(fieldSpec);
+
+                    MethodSpec.Builder methodSpec = MethodSpec.methodBuilder("body")
+                            .addModifiers(Modifier.PUBLIC)
+                            .addParameter(innerClassName, "body")
+                            .returns(ClassName.bestGuess(requestFormClassName))
+                            .addStatement("this.body = body")
+                            .addStatement("return this");
+
+                    if (enclosedElement.asType().getKind() == TypeKind.ARRAY) {
+                        methodSpec.varargs(true);
+                    }
+                    methodSpecs.add(methodSpec.build());
+                }
+            }
+        }
+
+        TypeSpec.Builder classBuilder = TypeSpec.classBuilder(requestFormClassName)
+                .addSuperinterface(superType)
+                .addModifiers(Modifier.PUBLIC)
+                .addFields(fieldSpecs)
+                .addMethods(methodSpecs);
+
+        if (innerClassSpec != null) {
+            classBuilder.addType(innerClassSpec);
+        }
+
+        JavaFile javaFile = JavaFile.builder(pacakageName, classBuilder.build())
+                .build();
+
+        try {
+            javaFile.writeTo(processingEnv.getFiler());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static MethodSpec createFactoryMethod(String requestFormClassName) {
