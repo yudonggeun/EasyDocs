6db07b99606257790d6cb2d7146ec8f03b20a0bb
yudonggeun <ydong98@gmail.com>
2025-01-07 16:04:58 +0900

    Schema -> ObjectType 변경

--- easy-docs-http/src/main/java/io/github/yudonggeun/http/schema/ObjectType.java
+++ easy-docs-http/src/main/java/io/github/yudonggeun/http/schema/ObjectType.java
@@ -1,17 +1,14 @@
 package io.github.yudonggeun.http.schema;
 
-import io.github.yudonggeun.http.JsonType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.TYPE})
-public @interface Schema {
+@Target(ElementType.TYPE)
+public @interface ObjectType {
 
-    JsonType type();
     String name() default "";
     String description() default "";
 }
