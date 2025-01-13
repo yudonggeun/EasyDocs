302e9a23f3b35bd1ccecfc60c3df21b2363eecea
yudonggeun <ydong98@gmail.com>
2025-01-13 11:52:25 +0900

    EasyDocsExtension AfterTestCallBack -> AfterAllCallBack 변경

--- easy-docs-http/src/main/java/io/github/yudonggeun/junit/EasyDocsExtension.java
+++ easy-docs-http/src/main/java/io/github/yudonggeun/junit/EasyDocsExtension.java
@@ -1,13 +1,13 @@
 package io.github.yudonggeun.junit;
 
 import io.github.yudonggeun.Document;
-import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class EasyDocsExtension implements AfterTestExecutionCallback {
+public class EasyDocsExtension implements AfterAllCallback {
 
     @Override
-    public void afterTestExecution(ExtensionContext context) throws Exception {
+    public void afterAll(ExtensionContext context) throws Exception {
         String openApi3 = Document.getOpenApi3(4);
         System.out.println(openApi3);
     }
